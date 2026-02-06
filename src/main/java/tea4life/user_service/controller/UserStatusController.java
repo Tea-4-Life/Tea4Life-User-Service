09e8b30706e4b3f7437d0722bc1de740929b38c1
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-06 22:03:55 +0700

    refactor(core): Adjust UserStatusController endpoint path
    The endpoint for checking user status was updated from `/api/v1/users/exists/{email}` to `/api/v1/users/public/users/exists/{email}` in UserStatusController.
    Additionally, added `eureka.instance.prefer-ip-address=true` to `application.properties` to ensure Eureka clients register with IP addresses instead of hostnames.

--- src/main/java/tea4life/user_service/controller/UserStatusController.java
+++ src/main/java/tea4life/user_service/controller/UserStatusController.java
@@ -3,10 +3,8 @@ package tea4life.user_service.controller;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tea4life.user_service.dto.base.ApiResponse;
 import tea4life.user_service.dto.response.UserStatusResponse;
@@ -17,14 +15,13 @@ import tea4life.user_service.service.UserStatusService;
  *
  **/
 @RestController
-@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserStatusController {
 
     UserStatusService userStatusService;
 
-    @GetMapping("/exists/{email}")
+    @GetMapping("/public/users/exists/{email}")
     public ApiResponse<UserStatusResponse> exists(@PathVariable String email) {
         return new ApiResponse<>(userStatusService.checkUserStatus(email));
     }
