0e4dccdd4ea5a61197adb4d9c41056d57944242e
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-16 19:33:31 +0700

    feat(service): Add PermissionService and RoleService implementations

---
+++ src/main/java/tea4life/user_service/service/impl/RoleServiceImpl.java
@@ -0,0 +1,27 @@
+package tea4life.user_service.service.impl;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tea4life.user_service.repository.PermissionRepository;
+import tea4life.user_service.repository.RoleRepository;
+
+/**
+ * Admin 2/16/2026
+ *
+ **/
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+@Transactional
+public class RoleServiceImpl {
+
+    RoleRepository roleRepository;
+    PermissionRepository permissionRepository;
+
+
+}
